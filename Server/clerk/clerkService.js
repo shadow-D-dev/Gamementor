35695e70f3bf74e37c8ed185204f10e23142b048
sanket <princesanket18@gmail.com>
2025-04-25 02:47:28 +0530

    Refactor: Use environment variable for video fetch API routes try 2

--- Server/clerk/clerkService.js
+++ Server/clerk/clerkService.js
@@ -1,3 +1,37 @@
+// import {Clerk} from '@clerk/clerk-sdk-node';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+
+
+// const clerkClient = Clerk({
+//     secretKey: process.env.CLERK_API_KEY,
+// });
+
+// export async function assignOrgAdminRole(email) {
+//   try {
+//     const users = await clerkClient.users.getUserList({ emailAddress: [email] }); 
+//     if (users.length > 0) {
+//       const userId = users[0].id;
+
+//       // Role assign करना publicMetadata के ज़रिए होता है
+//       await clerkClient.users.updateUser(userId, {
+//         publicMetadata: {
+//           role: 'admin'
+//         }
+//       });
+
+//       console.log("✅ User role updated to admin!");
+//     } else {
+//       console.log("❌ User not found.");
+//     }
+//   } catch (error) {
+//     console.error("❌ Error assigning role:", error);
+//   }
+// }
+
+
 import {Clerk} from '@clerk/clerk-sdk-node';
 import dotenv from 'dotenv';
 
@@ -6,7 +40,7 @@ dotenv.config();
 
 
 const clerkClient = Clerk({
-    secretKey: process.env.CLERK_API_KEY,
+    secretKey: process.env.CLERK_SECRET_KEY,
 });
 
 export async function assignOrgAdminRole(email) {
@@ -30,3 +64,5 @@ export async function assignOrgAdminRole(email) {
     console.error("❌ Error assigning role:", error);
   }
 }
+
+
